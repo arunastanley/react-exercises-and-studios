333dd961137103fba9991e0bd61d2a51b4fe6d6f
arunastanley <arunapaulus@gmail.com>
2024-08-18 23:01:01 -0500

    React Part1 exercises

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,16 @@
+import classes from './BookList.module.css';
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Latest Book Releases";
+   let book1 = "https://m.media-amazon.com/images/I/412JSB73D2L._AC_UF1000,1000_QL80_.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/51QLKvQCj-L.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/61YHUzQmuBL._AC_UF1000,1000_QL80_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} className={classes.images} alt="Lord of the Rings" />
+         <img src={book2} className={classes.images} alt="Miracles from heaven" />
+         <img src={book3} className={classes.images} alt="PS I Love You" />
       </div>      
    );
 }
--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,15 @@
+import classes from './ChoresList.module.css';
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+    const chores = ["Empty Dishwasher", "Complete prep work", "Buy groceries"];
+   return (<div>
+               <h3 className= {classes.choresHeading} >Todays chores</h3>
+               <ul>
+                  <li className= {classes.choresText}>{chores[0]}</li>
+                  <li className= {classes.choresText}>{chores[1]}</li>
+                  <li className= {classes.choresText}>{chores[2]}</li>
+               </ul>
+          </div>);
+ 
+      
+ 
 }
