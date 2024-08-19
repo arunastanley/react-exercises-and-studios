4c95925150694ae0b22250607c513d2b609e10f5
arunastanley <arunapaulus@gmail.com>
2024-08-18 23:17:27 -0500

    React Part1 exercises

---
+++ part1/exercises/src/components/HobbyLinks.jsx
@@ -0,0 +1,12 @@
+export default function HobbyLinks(){
+let hobbyLinks = ["https://www.lionbrand.com/pages/how-to-crochet?srsltid=AfmBOoqO9qaOJy9jY91XNhvJjY-HXRyTUP7zmW7W48DHma5HVrpBKAnG",
+"https://www.joyofbaking.com/", "https://onestroke.com/?srsltid=AfmBOoqAOJ5orZ09hr0iOec0e318Whf22h03c81HP7eC_1y9VxE7V2_O"];
+    return(
+        <div>
+            <a href="{hobbyLinks[0]}">Learn to crochet</a>
+            <a href="{hobbyLinks[1]}">Learn to bake</a>
+            <a href="{hobbyLinks[2}">Learn to paint</a>
+  
+        </div>  );
+       
+}
